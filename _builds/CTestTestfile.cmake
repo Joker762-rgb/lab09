18b1fd34a5af8194c5a248e224044dc65eb1c1f0
Joker762-rgb <Kuzmin2006.ru@yandex.ru>
2025-04-28 19:33:14 +0300

    Add files via upload

---
+++ _builds/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/luka/Joker762-rgb/workspace/projects/lab07
+# Build directory: /home/luka/Joker762-rgb/workspace/projects/lab07/_builds
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(check "/home/luka/Joker762-rgb/workspace/projects/lab07/_builds/check")
+set_tests_properties(check PROPERTIES  _BACKTRACE_TRIPLES "/home/luka/Joker762-rgb/workspace/projects/lab07/CMakeLists.txt;24;add_test;/home/luka/Joker762-rgb/workspace/projects/lab07/CMakeLists.txt;0;")
